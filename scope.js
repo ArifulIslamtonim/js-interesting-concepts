d9412471410403687df535be4ba0b1adb8045285
AriulIslamtonim <arifulislamtonim@gmail.com>
2022-02-25 12:13:59 +0600

    scope sj

---
+++ scope.js
@@ -0,0 +1,11 @@
+function add(first, second) {
+    const result = first + second;
+    if(result > 9){
+        var mood = 'happy';
+        // console.log(mood);
+    }
+    console.log(mood);
+    return result;
+}
+const sum = add(34, 4);
+console.log(sum);
